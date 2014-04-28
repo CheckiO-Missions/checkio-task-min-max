6b6c08a0313f80ec6557d742423cc68d093d3f44
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-04-28 16:13:35 +1200

    add tests

--- verification/tests.py
+++ verification/tests.py
@@ -1,4 +1,4 @@
-prepare = """
+init_code = """
 if not "min" in USER_GLOBAL:
     raise NotImplementedError("Where is 'min'?")
 if not "max" in USER_GLOBAL:
@@ -15,78 +15,38 @@ run_test = """RET['code_result'] = {}
 
 simple_max = "max(3, 2)"
 simple_min = "min(3, 2)"
-
 max_range = "max([1, 2, 0, 3, 4])"
-
 min_hello = "min('hello')"
-
 two_max_items = "max(2.2, 5.6, 5.9, key=int)"
-
 lambda_key = "min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1])"
+
+
+def prepare_test(test, answer):
+    return {
+            "test_code": {"python-3": init_code + run_test.format(test), "python-27": init_code + run_test.format(test)},
+            "show": {"python-3": test, "python-27": test},
+            "answer": answer}
+
 
 TESTS = {
     "1. Basics": [
-        {
-            "test_code": {
-                "python-3": prepare + run_test.format(simple_max),
-                "python-27": prepare + run_test.format(simple_max)
-            },
-            "show": {"python-3": simple_max, "python-27": simple_max},
-            "answer": 3
-        },
-        {
-            "test_code": {
-                "python-3": prepare + run_test.format(simple_min),
-                "python-27": prepare + run_test.format(simple_min)
-            },
-            "show": {"python-3": simple_min, "python-27": simple_min},
-            "answer": 2
-        },
-        {
-            "test_code": {
-                "python-3": prepare + run_test.format(max_range),
-                "python-27": prepare + run_test.format(max_range)
-            },
-            "show": {"python-3": max_range, "python-27": max_range},
-            "answer": 4
-        },
-        {
-            "test_code": {
-                "python-3": prepare + run_test.format(min_hello),
-                "python-27": prepare + run_test.format(min_hello)
-            },
-            "show": {"python-3": min_hello, "python-27": min_hello},
-            "answer": "e"
-        },
-        {
-            "test_code": {
-                "python-3": prepare + run_test.format(two_max_items),
-                "python-27": prepare + run_test.format(two_max_items)
-            },
-            "show": {"python-3": two_max_items, "python-27": two_max_items},
-            "answer": 5.6
-        },
-        {
-            "test_code": {
-                "python-3": prepare + run_test.format(lambda_key),
-                "python-27": prepare + run_test.format(lambda_key)
-            },
-            "show": {"python-3": lambda_key, "python-27": lambda_key},
-            "answer": [9, 0]
-        },
-
-
+        prepare_test("max(3, 2)", 3),
+        prepare_test("min(3, 2)", 2),
+        prepare_test("max([1, 2, 0, 3, 4])", 4),
+        prepare_test("min('hello')", "e"),
+        prepare_test("max(2.2, 5.6, 5.9, key=int)", 5.6),
+        prepare_test("min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1])", [9, 0])
     ],
-    # "Extra": [
-    #     {
-    #         "input": [6, 3],
-    #         "answer": 9,
-    #         "explanation": "6+3=?"
-    #     },
-    #     {
-    #         "input": [6, 7],
-    #         "answer": 13,
-    #         "explanation": "6+7=?"
-    #     }
-    # ]
+    "Extra": [
+        prepare_test("max([0])", 0),
+        prepare_test("min((9,))", 9),
+        prepare_test("max(range(6))", 5),
+        prepare_test("min({1, 2, 3, 4, -10})", -10),
+        prepare_test("max(set('djsaljldsklfjzx'))", "z"),
+        prepare_test("min(set('djsaljldsklfjzx'))", "a"),
+        prepare_test("max([1, 2, 3], [5, 6], [7], [0, 0, 0, 1])", [7]),
+        prepare_test("min([1, 2, 3], [5, 6], [7], [0, 0, 0, 10], key=sum)", [1, 2, 3]),
+        prepare_test("max(True, False, -1, key=lambda x: not x)", False),
+        prepare_test("min(True, False, -1)", -1),
+    ]
 }
