4fcf3ed423f197519c15bee8164df64ea38cf7c0
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-04-28 12:52:31 +1200

    base tests

--- verification/tests.py
+++ verification/tests.py
@@ -1,36 +1,92 @@
-"""
-TESTS is a dict with all you tests.
-Keys for this will be categories' names.
-Each test is dict with
-    "input" -- input data for user function
-    "answer" -- your right answer
-    "explanation" -- not necessary key, it's using for additional info in animation.
+prepare = """
+if not "min" in USER_GLOBAL:
+    raise NotImplementedError("Where is 'min'?")
+if not "max" in USER_GLOBAL:
+    raise NotImplementedError("Where is 'max'?")
+
+min = USER_GLOBAL['min']
+max = USER_GLOBAL['max']
 """
 
+run_test = """RET['code_result'] = {}
+"""
+
+###Basic
+
+simple_max = "max(3, 2)"
+simple_min = "min(3, 2)"
+
+max_range = "max([1, 2, 0, 3, 4])"
+
+min_hello = "min('hello')"
+
+two_max_items = "max(2.2, 5.6, 5.9, key=int)"
+
+lambda_key = "min([[1, 2], [3, 4], [9, 0]], key=lambda x: x[1])"
 
 TESTS = {
-    "Basics": [
+    "1. Basics": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "test_code": {
+                "python-3": prepare + run_test.format(simple_max),
+                "python-27": prepare + run_test.format(simple_max)
+            },
+            "show": {"python-3": simple_max, "python-27": simple_max},
+            "answer": 3
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
+            "test_code": {
+                "python-3": prepare + run_test.format(simple_min),
+                "python-27": prepare + run_test.format(simple_min)
+            },
+            "show": {"python-3": simple_min, "python-27": simple_min},
+            "answer": 2
+        },
+        {
+            "test_code": {
+                "python-3": prepare + run_test.format(max_range),
+                "python-27": prepare + run_test.format(max_range)
+            },
+            "show": {"python-3": max_range, "python-27": max_range},
+            "answer": 2
+        },
+        {
+            "test_code": {
+                "python-3": prepare + run_test.format(min_hello),
+                "python-27": prepare + run_test.format(min_hello)
+            },
+            "show": {"python-3": min_hello, "python-27": min_hello},
+            "answer": 2
+        },
+        {
+            "test_code": {
+                "python-3": prepare + run_test.format(two_max_items),
+                "python-27": prepare + run_test.format(two_max_items)
+            },
+            "show": {"python-3": two_max_items, "python-27": two_max_items},
+            "answer": 2
+        },
+        {
+            "test_code": {
+                "python-3": prepare + run_test.format(lambda_key),
+                "python-27": prepare + run_test.format(lambda_key)
+            },
+            "show": {"python-3": two_max_items, "python-27": lambda_key},
+            "answer": 2
+        },
+
+
     ],
-    "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
-        },
-        {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
-    ]
+    # "Extra": [
+    #     {
+    #         "input": [6, 3],
+    #         "answer": 9,
+    #         "explanation": "6+3=?"
+    #     },
+    #     {
+    #         "input": [6, 7],
+    #         "answer": 13,
+    #         "explanation": "6+7=?"
+    #     }
+    # ]
 }
